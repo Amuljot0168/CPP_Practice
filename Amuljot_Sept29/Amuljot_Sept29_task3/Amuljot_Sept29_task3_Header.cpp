6009f872ee2d05033f3c94285a50442a833ddd35
Amuljot Singh <105152271+Amuljot0168@users.noreply.github.com>
2025-09-29 16:11:49 +0530

    Pushing September 29 Practice Questions Solutions

---
+++ Amuljot_Sept29/Amuljot_Sept29_task3/Amuljot_Sept29_task3_Header.cpp
@@ -0,0 +1,29 @@
+#include "Amuljot_Sept29_task3_Header.h"
+
+// Helper Function for evaluate function implementation
+int doubleTheValue(int number)
+{
+    return 2 * number;
+}
+
+// Exchange function to exchange the values of double type numbers
+void exchange(double *x, double *y)
+{
+    double temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// Exchange function to exchange the values of float type numbers
+void exchange(float *x, float *y)
+{
+    float temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// Function which takes function pointer as parameter
+int evaluate(int number, int (*poly)(int))
+{
+    return poly(number);
+}
